c33603d3107c554b016e840c944ef667ac24d992
Troy Martin <beef.erikson.studios@gmail.com>
2026-02-04 20:30:08 -0500

    chapter 2.4 completed

--- ch2/2_4.cpp
+++ ch2/2_4.cpp
@@ -11,5 +11,17 @@ int main() {
          << "\nnn squared == " << n * n
          << "\nhalf of n == " << n / 2
          << "\nsquare root of n == " << sqrt(n)
-         << "\n"; 
+         << "\n\n"; 
+
+    cout << "Please enter two names\n";
+    string first;
+    string second;
+    cin >> first >> second;
+    
+    if (first == second) 
+        cout << "that's the same name twice\n";
+    if (first < second)
+        cout << first << " is alphabetically before " << second << "\n";
+    if (first > second)
+        cout << first << " is alphabetically after " << second << "\n";
 }
