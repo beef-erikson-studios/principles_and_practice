64df2d507be4d7369036808bead940a4156cc461
Troy Martin <beef.erikson.studios@gmail.com>
2026-02-06 00:42:11 -0500

    chapter 2 complete.

--- ch2/exercise2.cpp
+++ ch2/exercise2.cpp
@@ -1,7 +1,15 @@
 #include "PPPheaders.h"
 
+// Miles to Kilometers
 int main() {
-    
+    double miles = 0;
+    double kilometers = 1.609;
+
+    cout << "Enter the miles to convert to kilometers: ";
+    cin >> miles;
+
+    kilometers *= miles;
+    cout << miles << " miles is " << kilometers << " kilometers.\n\n";
 
     return 0;
 }
